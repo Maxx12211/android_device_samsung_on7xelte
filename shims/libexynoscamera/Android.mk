6988ae4e2cb7b74bb3253b1072f0ccd250c65019
Maxx1221 <somanathcharan9@gmail.com>
2022-07-17 17:30:16 +0530

    shims: libexynoscamera: Android.mk
     remove libgui reference to fix with BOARD_VNDK_VERSION := current.

--- shims/libexynoscamera/Android.mk
+++ shims/libexynoscamera/Android.mk
@@ -22,7 +22,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_C_INCLUDES := frameworks/native/include
 
-LOCAL_SHARED_LIBRARIES := libgui libui
+LOCAL_SHARED_LIBRARIES := libui
 
 LOCAL_MODULE := libexynoscamera_shim
 LOCAL_MODULE_TAGS := optional
