d1b457712d07dbd403e730828b303c4df311219f
Maxx1221 <somanathcharan9@gmail.com>
2022-07-17 15:48:41 +0530

    graphics composer: use passthrough implementation

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
