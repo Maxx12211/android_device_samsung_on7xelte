c17c6d39dceeb1fd9799fb3742dbdd44fb4f8c9b
Maxx1221 <somanathcharan9@gmail.com>
2022-07-17 15:44:14 +0530

    uprev audio.

--- device.mk
+++ device.mk
@@ -30,9 +30,10 @@ TARGET_SCREEN_HEIGHT := 1920
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
     audio.primary.default \
     audio.primary.universal7870 \
